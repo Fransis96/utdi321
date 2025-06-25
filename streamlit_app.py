726aa2db1ef2baaa88c36703767f2081f7115266
Fransis96 <fransiskus.agustinus@students.utdi.ac.id>
2025-06-26 02:58:04 +0700

    Restore

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,36 @@
 import streamlit as st
 import joblib
-import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-model_dnn = load_model('./model/dnn_model_v1.keras')
+# Load semua model dan vectorizer
+model_lr = joblib.load('./model/lr_model_v1.joblib')
+vectorizer_lr = joblib.load('./model/tfidf_v1.joblib')
 
-tokenizer_dnn = joblib.load('./model/tokenizer_dnn_v1.joblib')
-label_encoder_dnn = joblib.load('./model/label_encoder_v1.joblib')
+model_svc = joblib.load('./model/svc_model_v3.joblib')
+svc_vectorizer = joblib.load('./model/tfidf_svc_v3.joblib')
 
-maxlen = 100  # Panjang input teks
+# Judul halaman
+st.title("🕵️ Klasifikasi Sentimen Teks")
 
-# ======== UI STREAMLIT ========
-st.title("🧠 Prediksi Sentimen (Model DNN)")
+# Pilihan model
+model_choice = st.selectbox("Pilih model yang akan digunakan:", ["Logistic Regression", "SVC"])
 
+# Input dari user
 teks_input = st.text_area("Masukkan teks untuk dianalisis:")
 
-if st.button("Prediksi"):
+# Tombol prediksi
+if st.button("Prediksi Sentimen"):
     if not teks_input.strip():
-        st.warning("Teks tidak boleh kosong 😅")
+        st.warning("Teks tra boleh kosong 😏")
     else:
-        teks_bersih = teks_input.lower().strip()
-        seq = tokenizer_dnn.texts_to_sequences([teks_bersih])
-        padded = pad_sequences(seq, maxlen=maxlen)
-        prob = model_dnn.predict(padded, verbose=0)[0][0]
-        label = 1 if prob > 0.5 else 0
-        prediksi = label_encoder_dnn.inverse_transform([label])[0]
+        teks_bersih = teks_input.lower()
+
+        # Tentukan model & vectorizer
+        if model_choice == "Logistic Regression":
+            X = vectorizer_lr.transform([teks_bersih])
+            prediksi = model_lr.predict(X)[0]
+        else:
+            X = svc_vectorizer.transform([teks_bersih])
+            prediksi = model_svc.predict(X)[0]
+
+        # Tampilkan hasil
         st.success(f"Hasil Prediksi: **{prediksi.upper()}**")
